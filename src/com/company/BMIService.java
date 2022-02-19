89d6a5a8d556a28d00c5089d59dbcf6003fb9565
LTH4823 <peter4823zx@naver.com>
2022-02-19 10:58:33 +0900

    Initial commit

---
+++ src/com/company/BMIService.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class BMIService {
+
+    //이름 인자(parameter) 결과 타입
+    public double calculate(Person person){
+
+        return 0.1;
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BMIService service = new BMIService();
+
+        Person p1 = new Person(180,60);
+
+        double result = service.calculate(p1);
+
+        System.out.println(result);
+    }
+}
